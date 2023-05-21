1bce30ca92f1c4cd848613a74aa36b9219dcd21d
Eduardisamuel <123224329+Eduardisamuel@users.noreply.github.com>
2023-05-21 16:38:56 -0400

    deleted info

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-require('dotenv').config()
-
-// remove this once you confirm it works
-// console.log(process.env.JWT_SECRET)
-// like, seriously. go delete that!
-
-// EVERYTHING ELSE
 
 const PORT = process.env.PORT || 3000
 const express = require('express')
